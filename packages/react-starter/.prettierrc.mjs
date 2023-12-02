5cc3ab997177aac8b082b6075d91abd4de668a0c
gaoxiu <gaoxiu333@gmail.com>
2023-12-02 22:48:01 +0800

    feat: 将prettier集成到eslint中

--- packages/react-starter/.prettierrc.mjs
+++ packages/react-starter/.prettierrc.mjs
@@ -2,6 +2,11 @@
  * @type {import('prettier').Config}
  */
 const config = {
-  printWidth: 80,
-  tabWidth: 4,
+  semi: false, // 分号
+  printWidth: 80, // 一行的字符数，如果超过会进行换行
+  tabWidth: 4, // 缩进的空格数
+  singleQuote: true, // 使用单引号
+  // trailingComma: 'none', // 在对象或数组最后一个元素后面是否加逗号
+  // bracketSpacing: true, // 对象大括号直接是否有空格
 };
+export default config;
